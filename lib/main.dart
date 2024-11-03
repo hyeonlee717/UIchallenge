7a6ad21422775cc0012cbffbc89983fee224e6d6
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-03 22:08:56 +0900

    lesson_2_5

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:uichallenge/widjets/button.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Color(0xFF181818),
+      backgroundColor: const Color(0xFF181818),
       body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 40,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 50),
+            const SizedBox(height: 80),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text(
+                    const Text(
                       'Hey, Selena',
                       style: TextStyle(
                         color: Colors.white,
@@ -42,6 +46,43 @@ class App extends StatelessWidget {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style: TextStyle(
+                color: Colors.white.withOpacity(0.8),
+                fontSize: 22,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              '\$5 194 482',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 44,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Button(
+                    text: 'Transfer',
+                    bgColor: Color(0xfff1b33b),
+                    textColor: Colors.black),
+                Button(
+                    text: 'Request',
+                    bgColor: Color(0xff1f2123),
+                    textColor: Colors.white),
+              ],
+            )
           ],
         ),
       ),
